08bb6c4f0e48b1d44243a8b9674cb1b71797b273
muraliv2002 <reddymurali581@gmail.com>
2024-07-26 16:05:56 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Attendance from "./Attendance";
+import Classes from "./Classes";
+import Placements from "./Placements";
+function App(){
+return(
+  <>
+  <h1>
+    Kodnest App
+    <Classes/>
+    <Attendance/>
+    </h1>
+  </>
+  
+);
 }
-
 export default App;
